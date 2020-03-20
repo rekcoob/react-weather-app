ac37fd12187c926d7a6361008fc59f563b8342bf
ImDakoty <shanoun@webpack.sk>
2020-03-20 18:20:02 +0100

    fix location bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Titles from './components/Titles';
-import Form from './components/Form';
-import Weather from './components/Weather';
+import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
 const API_KEY = "683f35c2552f535f1a68b77c91bf3b28";
 
@@ -13,24 +13,37 @@ class App extends React.Component {
     humidity: undefined,
     description: undefined,
     error: undefined
-  }
+  };
 
-  getPocasie = async (e) => {
+  getPocasie = async e => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
+    );
     const data = await api_call.json();
     if (city && country) {
-      this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: ''
-      })
+      if (data.cod == 404) {
+        this.setState({
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: "Input doesn't match any known location!"
+        });
+      } else {
+        this.setState({
+          temperature: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          humidity: data.main.humidity,
+          description: data.weather[0].description,
+          error: ""
+        });
+      }
     } else {
       this.setState({
         temperature: undefined,
@@ -38,11 +51,10 @@ class App extends React.Component {
         country: undefined,
         humidity: undefined,
         description: undefined,
-        error: 'Please enter the values'
-      })
-
+        error: "Please enter the values"
+      });
     }
-  }
+  };
   render() {
     return (
       <div>
@@ -69,11 +81,8 @@ class App extends React.Component {
           </div>
         </div>
       </div>
-
-    )
+    );
   }
 }
-
-
 
 export default App;
